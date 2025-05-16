8a6036bd92eefee9c3f7da127a1500d8d534aeca
mish0908 <mish0908@gmail.com>
2025-05-16 11:46:36 -0400

    more data....

--- app/astrology.js
+++ app/astrology.js
@@ -3,7 +3,7 @@ const keys = require('./keys/api_keys');
 
 var options = {
     'method': 'POST',
-    'url': 'https://astroapi-4.divineapi.com/western-api/v1/general-sign-report/sun',
+    'url': 'https://astroapi-4.divineapi.com/western-api/v1/general-sign-report/moon',
     'headers': {
     'Authorization': `Bearer ${keys.AUTH_TOKEN}`
   },
